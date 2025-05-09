256a0a46b680d88da132f2c0594cdcf572e924d9
AleZane29 <alessio.zanella29@gmail.com>
2025-05-09 15:09:46 +0200

    Test primi 3 numeri close #8

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -7,5 +7,8 @@ package it.unipd.mtss;
 public class RomanPrinter {
   public static void main( String[] args )
     {
+      public static String print(int num) {
+        return ""; //Implementare ASCII
+    }
     }
 }
