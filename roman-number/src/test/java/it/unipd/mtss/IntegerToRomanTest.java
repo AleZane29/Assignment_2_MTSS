686fbfbb243e06a1f680f65efefecd2235b2d878
AleZane29 <alessio.zanella29@gmail.com>
2025-05-09 13:32:06 +0200

    Test primi 3 numeri close #8

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,17 @@
 // Alessio Zanella 2101079
 // Leonardo Salviato 2101086
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+  }
